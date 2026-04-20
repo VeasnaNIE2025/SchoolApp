cf9affa7b0d8d5532383f923c5dc41ae997d6f98
VeasnaEn <man.veasna.hs@moeys.gov.kh>
2026-04-20 16:25:29 +0700

    fix2

--- src/App.jsx
+++ src/App.jsx
@@ -1,69 +1,38 @@
-// // App.jsx
-// import { BrowserRouter, Routes, Route } from "react-router-dom";
-// import "./App.css";
-// import "./index.css";
-// import HomePage from "./page/home/HomePage";
-// import NewsPage from "./page/news/NewsPage";
-// import MainLayout from "./components/Layout/MainLayout";
-// import IctPage from "./page/ICT/IctPage";
-// import MainLayout1 from "./components/Layout/MainLayout1";
-// import ContactPage from "./page/contact/ContactPage";
-// import AboutMePage from "./page/aboutme/AboutMePage";
-// import VidoesPage from "./page/videos/VideosPage";
-// function App() {
-//   return (
-//     <BrowserRouter>
-//           <Routes>
-//             <Route element={<MainLayout/>}> 
-//               <Route path="/" element={<HomePage/>} />
-//               <Route path="/contact" element={<ContactPage/>} />
-//             </Route>
-//             <Route element={<MainLayout1/>}>
-//               <Route path="/news" element={<NewsPage />} />
-//               <Route path="/ict" element={<IctPage/>} />
-//               <Route path="/videos" element={<VidoesPage/>} />
-//               <Route path="/aboutme" element={<AboutMePage/>} />
-//               <Route path="*" element={<h1>Error Route</h1>}/>
-//             </Route>
-//           </Routes>
-//     </BrowserRouter>
-//   );
-// }
-
-// export default App;
-
+// App.jsx
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import "./App.css";
 import "./index.css";
 
 import HomePage from "./page/home/HomePage";
 import NewsPage from "./page/news/NewsPage";
-import MainLayout from "./components/Layout/MainLayout";
-import MainLayout1 from "./components/Layout/MainLayout1";
 import IctPage from "./page/ICT/IctPage";
+import VideosPage from "./page/videos/VideosPage";     // ← កែឈ្មោះ
 import ContactPage from "./page/contact/ContactPage";
 import AboutMePage from "./page/aboutme/AboutMePage";
-import VideosPage from "./page/videos/VideosPage";
+
+import MainLayout from "./components/Layout/MainLayout";
+import MainLayout1 from "./components/Layout/MainLayout1";
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-
+        {/* Layout 1: Home & Contact */}
         <Route element={<MainLayout />}>
           <Route path="/" element={<HomePage />} />
           <Route path="/contact" element={<ContactPage />} />
         </Route>
 
+        {/* Layout 2: ទំព័រផ្សេងៗ */}
         <Route element={<MainLayout1 />}>
           <Route path="/news" element={<NewsPage />} />
           <Route path="/ict" element={<IctPage />} />
           <Route path="/videos" element={<VideosPage />} />
           <Route path="/aboutme" element={<AboutMePage />} />
+
+          {/* Catch all 404 សម្រាប់ទំព័រដែលមិនមាន */}
+          <Route path="*" element={<h1>404 - Page Not Found</h1>} />
         </Route>
-
-        <Route path="*" element={<h1>Error Route</h1>} />
-
       </Routes>
     </BrowserRouter>
   );
