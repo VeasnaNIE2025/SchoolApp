11a05c02dd32e535b67af29536d410162348a8e8
VeasnaEn <man.veasna.hs@moeys.gov.kh>
2026-04-20 13:26:00 +0700

    fix Videos

--- src/App.jsx
+++ src/App.jsx
@@ -1,31 +1,70 @@
-// App.jsx
+// // App.jsx
+// import { BrowserRouter, Routes, Route } from "react-router-dom";
+// import "./App.css";
+// import "./index.css";
+// import HomePage from "./page/home/HomePage";
+// import NewsPage from "./page/news/NewsPage";
+// import MainLayout from "./components/Layout/MainLayout";
+// import IctPage from "./page/ICT/IctPage";
+// import MainLayout1 from "./components/Layout/MainLayout1";
+// import ContactPage from "./page/contact/ContactPage";
+// import AboutMePage from "./page/aboutme/AboutMePage";
+// import VidoesPage from "./page/videos/VideosPage";
+// function App() {
+//   return (
+//     <BrowserRouter>
+//           <Routes>
+//             <Route element={<MainLayout/>}> 
+//               <Route path="/" element={<HomePage/>} />
+//               <Route path="/contact" element={<ContactPage/>} />
+//             </Route>
+//             <Route element={<MainLayout1/>}>
+//               <Route path="/news" element={<NewsPage />} />
+//               <Route path="/ict" element={<IctPage/>} />
+//               <Route path="/videos" element={<VidoesPage/>} />
+//               <Route path="/aboutme" element={<AboutMePage/>} />
+//               <Route path="*" element={<h1>Error Route</h1>}/>
+//             </Route>
+//           </Routes>
+//     </BrowserRouter>
+//   );
+// }
+
+// export default App;
+
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import "./App.css";
 import "./index.css";
+
 import HomePage from "./page/home/HomePage";
 import NewsPage from "./page/news/NewsPage";
 import MainLayout from "./components/Layout/MainLayout";
-import IctPage from "./page/ICT/IctPage";
 import MainLayout1 from "./components/Layout/MainLayout1";
+import IctPage from "./page/ICT/IctPage";
 import ContactPage from "./page/contact/ContactPage";
 import AboutMePage from "./page/aboutme/AboutMePage";
-import VidoesPage from "./page/videos/VideosPage";
+import VideosPage from "./page/videos/VideosPage";
+
 function App() {
   return (
     <BrowserRouter>
-          <Routes>
-            <Route element={<MainLayout/>}> 
-              <Route path="/" element={<HomePage/>} />
-              <Route path="/contact" element={<ContactPage/>} />
-            </Route>
-            <Route element={<MainLayout1/>}>
-              <Route path="/news" element={<NewsPage />} />
-              <Route path="/ict" element={<IctPage/>} />
-              <Route path="/videos" element={<VidoesPage/>} />
-              <Route path="/aboutme" element={<AboutMePage/>} />
-              <Route path="*" element={<h1>Error Route</h1>}/>
-            </Route>
-          </Routes>
+      <Routes>
+
+        <Route element={<MainLayout />}>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/contact" element={<ContactPage />} />
+        </Route>
+
+        <Route element={<MainLayout1 />}>
+          <Route path="/news" element={<NewsPage />} />
+          <Route path="/ict" element={<IctPage />} />
+          <Route path="/videos" element={<VideosPage />} />
+          <Route path="/aboutme" element={<AboutMePage />} />
+        </Route>
+
+        <Route path="*" element={<h1>Error Route</h1>} />
+
+      </Routes>
     </BrowserRouter>
   );
 }
