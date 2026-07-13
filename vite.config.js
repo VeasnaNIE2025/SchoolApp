24d0f4f3322032c51088cfa9d2a14438eb805a64
MAN VEASN <manveasna20@gmail.com>
2026-07-13 12:38:11 +0700

    Fix PDF

--- vite.config.js
+++ vite.config.js
@@ -1,3 +1,24 @@
+// import { defineConfig } from 'vite'
+// import react from '@vitejs/plugin-react'
+
+// export default defineConfig({
+//   plugins: [
+//     react(),
+//   ],
+//   server: {
+//     historyApiFallback: true, // ← Add this line
+//   },
+//   build: {
+//     chunkSizeWarningLimit: 1000, // kB — លើកកម្រិត warning ពី 500 ទៅ 1000
+//     rollupOptions: {
+//       output: {
+//         manualChunks: {
+//           'pdf-export': ['jspdf', 'html2canvas'], // ញែក libraries ធំៗទៅ chunk ដាច់ដោយឡែក
+//         },
+//       },
+//     },
+//   },
+// })
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
@@ -12,8 +33,10 @@ export default defineConfig({
     chunkSizeWarningLimit: 1000, // kB — លើកកម្រិត warning ពី 500 ទៅ 1000
     rollupOptions: {
       output: {
-        manualChunks: {
-          'pdf-export': ['jspdf', 'html2canvas'], // ញែក libraries ធំៗទៅ chunk ដាច់ដោយឡែក
+        manualChunks(id) {
+          if (id.includes('jspdf') || id.includes('html2canvas')) {
+            return 'pdf-export'
+          }
         },
       },
     },
